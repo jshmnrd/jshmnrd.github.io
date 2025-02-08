2422816e00123d9a92740ca3d01925918988be50
Josh Maynard <102846760+jshmnrd@users.noreply.github.com>
2025-02-07 20:51:31 -0500

    Create blog-search.js

---
+++ plug-ins/blog-search.js
@@ -0,0 +1,19 @@
+// Credit to Yordi https://shorturl.at/cvVCe
+if (document.querySelector(".blog-posts") && document.body.classList.contains("blog")) {
+  document.querySelector("main").insertBefore(
+    Object.assign(
+      document.createElement("input"), {
+      type: "text",
+      id: "searchInput",
+      placeholder: "Search...",
+      style: "display: block;",
+      oninput: (event) => {
+        document.querySelectorAll(".blog-posts li").forEach((post) => {
+          post.style.display = post.textContent.toLowerCase().includes(event.target.value.toLowerCase()) ? "" : "none";
+        })
+      }
+    }),
+    document.querySelector(".blog-posts")
+  );
+} 
+</script>
